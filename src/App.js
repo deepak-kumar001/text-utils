5803d4911b4749bed5f730f2e18c4d6150739f53
Deepak Kumar <129864491+deepak-kumar001@users.noreply.github.com>
2023-08-12 17:20:55 +0530

    added alerts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
 
+let timer;
 function App() {
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  const [mode, setmode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = '#1a164e';
+      document.body.style.color = 'white';
+      showAlert("Dark mode Enabled", "success");
+      // document.title = "TextUtils - Home";
+    }
+    else {
+      setmode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light mode Enabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}></Navbar>
+      <Alert alert={alert} />
+      <div className="container">
+        <TextForm heading="Enter the Text below to Analyze" mode={mode} showAlert={showAlert}></TextForm>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState("");
+    const handleUpperClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UPPER case.","success")
+
+    };
+    const handleLowerClick = () => {
+        // console.log(text);
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lower case.","success")
+    };
+    const handleExtractCombo = () => {
+        // let pattern = /[\w@.+-]+:[\w!@#$%*.+-]+/gi;
+        let pattern = /[\w@.+-]+:\S+/g;
+        let newText = text.match(pattern)
+        if (newText) {
+            newText = newText.join('\n');
+            // console.log(newText);
+            setText(newText);
+            props.showAlert("Combo Extracted Successfully.","success")
+        }
+        else {
+            setText("");
+            props.showAlert("No Combo Found.","warning")
+
+        }
+    };
+    const handleSaveTxt = () => {
+        if(text == ""){
+            props.showAlert("There is nothing to save. Please input some text.","warning");
+            return
+        }
+        const element = document.createElement('a');
+        const file = new Blob([text], { type: 'text/plain' });
+        element.href = URL.createObjectURL(file);
+        element.download = 'mytextfile.txt';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    };
+    const handleReset = () => {
+        setText("");
+        props.showAlert("Cleared the Text Area.","success")
+
+    };
+
+    const onChangeEvent = (event) => {
+        setText(event.target.value)
+    }
+    return (
+        <>
+            <div>
+                <h2 className='mt-3'>{props.heading}</h2>
+                <div className="mb-3">
+                    <textarea className="form-control" style={{backgroundColor:props.mode==='light'?'white':'#4e3939',color:props.mode==='light'?'black':'white'}} placeholder="Enter your text here" placeholderTextColor={props.mode==='dark'?'white':'black'} value={text} onChange={onChangeEvent} id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpperClick} >Convert To Upper Case</button>
+                <button className="btn btn-primary mx-1" onClick={handleLowerClick} >Convert To Lower Case</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtractCombo} >Extract Combo</button>
+                <button className="btn btn-primary mx-1" onClick={handleSaveTxt} >Save Txt</button>
+                <button className="btn btn-primary mx-1" onClick={handleReset} >Reset</button>
+            </div>
+            <h2 className='mt-2'>Text Summary</h2>
+            <p>{text.length>0?text.trim().split(/\s+|\n+/).length:0} words, {text.length} characters</p>
+        </>
+    )
+}
