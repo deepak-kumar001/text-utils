708056e0edeb62fc17a9215cb0875f212308635e
Deepak Kumar <129864491+deepak-kumar001@users.noreply.github.com>
2023-08-13 14:13:26 +0530

    added more buttons

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -57,13 +57,14 @@ export default function TextForm(props) {
             <div>
                 <h2 className='mt-3'>{props.heading}</h2>
                 <div className="mb-3">
-                    <textarea className="form-control" style={{backgroundColor:props.mode==='light'?'white':'#4e3939',color:props.mode==='light'?'black':'white'}} placeholder="Enter your text here" placeholderTextColor={props.mode==='dark'?'white':'black'} value={text} onChange={onChangeEvent} id="exampleFormControlTextarea1" rows="8"></textarea>
+                    <textarea className={`form-control ${props.mode === 'light' ? 'placeholder-light' : 'placeholder-dark'}`} style={{backgroundColor:props.mode==='light'?'white':'rgb(45 38 133)',color:props.mode==='light'?'black':'white'}} placeholder="Enter your text here" value={text} onChange={onChangeEvent} id="exampleFormControlTextarea1" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpperClick} >Convert To Upper Case</button>
-                <button className="btn btn-primary mx-1" onClick={handleLowerClick} >Convert To Lower Case</button>
-                <button className="btn btn-primary mx-1" onClick={handleExtractCombo} >Extract Combo</button>
-                <button className="btn btn-primary mx-1" onClick={handleSaveTxt} >Save Txt</button>
-                <button className="btn btn-primary mx-1" onClick={handleReset} >Reset</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpperClick} >Convert To Upper Case</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLowerClick} >Convert To Lower Case</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtractCombo} >Extract Combo</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy to Clipboard</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleSaveTxt} >Save Txt</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleReset} >Reset</button>
             </div>
             <h2 className='mt-2'>Text Summary</h2>
             <p>{text.length>0?text.trim().split(/\s+|\n+/).length:0} words, {text.length} characters</p>
